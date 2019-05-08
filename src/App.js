601d1553e22b75e3e43f29c60b663db46b465b41
Jonathan Freeman <freethejazz@gmail.com>
2019-05-08 18:05:29 -0500

    Initial post

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'react-vis/dist/style.css';
+import { dsv } from 'd3-fetch';
+import moment from 'moment';
+import { XYPlot, VerticalBarSeries, XAxis, YAxis } from 'react-vis';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import dataUrl from './data/Popular_Baby_Names.csv';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+    //get data
+    dsv(",", dataUrl, (d) => {
+      return {
+        yearOfBirth: +d['Year of Birth'],
+        gender: d['Gender'],
+        ethnicity: d['Ethnicity'],
+        firstName: d['Child\'s First Name'],
+        count: +d['Count'],
+        rank: +d['Rank'],
+      };
+    }).then((data) => this.setState({data}));
+  }
+  render() {
+    const {
+      data
+    } = this.state;
+    const totalBabiesByYear = Object.entries(data.reduce((acc, row) => {
+      if(row.yearOfBirth in acc) {
+        acc[row.yearOfBirth] = acc[row.yearOfBirth] + row.count
+      } else {
+        acc[row.yearOfBirth] = row.count
+      }
+      return acc;
+    }, {})).map(([k, v]) => ({x: +k, y: v}));
+    return (
+      <div className="App">
+        <XYPlot
+          width={600}
+          height={600}
+          margin={{
+            left: 70
+          }}
+          xType="ordinal"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <VerticalBarSeries
+            data={totalBabiesByYear}
+          />
+          <XAxis />
+          <YAxis />
+        </XYPlot>
+      </div>
+    );
+
+  }
 }
 
 export default App;
